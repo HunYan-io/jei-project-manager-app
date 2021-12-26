6c09bf05d3e5eae935b283e10e0bcb760cb573ac
hanabenasker <hanabenasker@insat.u-carthage.tn>
2021-12-26 12:15:18 +0100

    added task service and modified task model

--- lib/models/task.dart
+++ lib/models/task.dart
@@ -1,34 +1,38 @@
-import 'package:flutter/cupertino.dart';
-
 class Task {
-  int? id;
-  String name;
-  String description;
-  DateTime deadline;
-  String status;
+  final int? id;
+  final String name;
+  final String project;
+  final String description;
+  final DateTime deadline;
+  final String status;
 
-  Task({
-    this.id,
-    required this.deadline,
-    required this.description,
-    required this.status,
-    required this.name,
-  });
+  Task(
+      {this.id,
+      required this.name,
+      required this.project,
+      required this.description,
+      required this.deadline,
+      required this.status});
 
-  factory Task.fromJson(Map<String, dynamic> entrer) {
-    return Task(
-      id: entrer["id"],
-      name: entrer["name"],
-      description: entrer["description"],
-      deadline: entrer["deadline"],
-      status: entrer["status"],
-    );
+  factory Task.fromJson(Map<String, dynamic> task) => Task(
+      id: task["id"],
+      name: task["name"],
+      project: task["project"],
+      description: task["description"],
+      deadline: DateTime.parse(task["deadline"]),
+      status: task["task"]);
+
+  Map<String, dynamic> toJson() {
+    final task = {
+      "name": name,
+      "project": project,
+      "description": description,
+      "deadline": deadline.toString(),
+      "status": status
+    };
+    if (id != null) {
+      task["id"] = id! as String;
+    }
+    return task;
   }
-
-  Map<String, dynamic> toJson() => {
-        "name": name,
-        "deadline": deadline,
-        "description": description,
-        "status": status,
-      };
 }
--- lib/services/task_service.dart
+++ lib/services/task_service.dart
@@ -1,21 +1,57 @@
 import 'dart:convert';
 
+import 'package:jei_project_manager_app/config.dart';
 import 'package:jei_project_manager_app/models/task.dart';
-import 'package:http/http.dart' as http;
+import 'package:jei_project_manager_app/services/http_service.dart';
 
 class TaskServices {
-  static Future<Task> getTasks(int ID) async {
-    var client = http.Client();
-    var url = Uri.parse(
-        'https://my-json-server.typicode.com/hanabenasker/ollert/' +
-            ID.toString());
-
-    var response = await client.get(url);
-
-    if (response.statusCode == 200) {
-      return Task.fromJson(jsonDecode(response.body));
+  Future<List<Task>> getTasks(int id) async {
+    final response = await httpService.get(
+      Uri.parse(Config.apiURL + "/projects/$id/tasks"),
+    );
+    if (response.statusCode < 400) {
+      final data = List<Map<String, dynamic>>.from(json.decode(response.body));
+      return data.map((task) => Task.fromJson(task)).toList();
     } else {
-      throw Exception("Failed to load task");
+      final data = json.decode(response.body) as Map<String, dynamic>;
+      throw Exception(data["error"]);
+    }
+  }
+
+  Future<bool> postTask(Task task, int id) async {
+    final response = await httpService.post(
+      Uri.parse(Config.apiURL + "/projects/$id/tasks"),
+      body: json.encode(task.toJson()),
+    );
+    final data = json.decode(response.body) as Map<String, dynamic>;
+    if (response.statusCode < 400) {
+      return data["success"];
+    } else {
+      throw Exception(data["message"]);
+    }
+  }
+
+  Future<bool> deleteTask(int id) async {
+    final response = await httpService.delete(
+      Uri.parse(Config.apiURL + "/task/$id"),
+    );
+    final data = json.decode(response.body) as Map<String, dynamic>;
+    if (response.statusCode < 400) {
+      return data["success"];
+    } else {
+      throw Exception(data["message"]);
+    }
+  }
+
+  Future<bool> updateTask(int id) async {
+    final response = await httpService.put(
+      Uri.parse(Config.apiURL + "/task/$id"),
+    );
+    final data = json.decode(response.body) as Map<String, dynamic>;
+    if (response.statusCode < 400) {
+      return data["success"];
+    } else {
+      throw Exception(data["message"]);
     }
   }
 }
