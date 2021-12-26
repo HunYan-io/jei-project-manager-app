e51712719fd9d1c2f5b807c80116af85bc3d6daa
Hana Ben Asker <36458363+hanabenasker@users.noreply.github.com>
2021-12-26 19:31:31 +0100

    added task details screen (#3)
    * added task details screen
    
    * added task service and modified task model
    
    * Delete Task.dart
    
    * Update lib/models/task.dart
    
    Co-authored-by: Heni Yangui <heni.yangui@insat.ucar.tn>
    
    * update task model and service
    
    * Update lib/models/task.dart
    
    Co-authored-by: Heni Yangui <heni.yangui@insat.ucar.tn>

---
+++ lib/models/task.dart
@@ -0,0 +1,37 @@
+class Task {
+  final int? id;
+  final String name;
+  final int project;
+  final String description;
+  final DateTime deadline;
+  final String status;
+
+  Task(
+      {this.id,
+      required this.name,
+      required this.project,
+      required this.description,
+      required this.deadline,
+      required this.status});
+
+  factory Task.fromJson(Map<String, dynamic> task) => Task(
+      id: task["id"],
+      name: task["name"],
+      project: task["projectId"],
+      description: task["description"],
+      deadline: DateTime.parse(task["deadline"]),
+      status: task["task"]);
+
+  Map<String, dynamic> toJson() {
+    final task = {
+      "name": name,
+      "description": description,
+      "deadline": deadline.toString(),
+      "status": status
+    };
+    if (id != null) {
+      task["id"] = id!;
+    }
+    return task;
+  }
+}
---
+++ lib/services/task_service.dart
@@ -0,0 +1,58 @@
+import 'dart:convert';
+
+import 'package:jei_project_manager_app/config.dart';
+import 'package:jei_project_manager_app/models/task.dart';
+import 'package:jei_project_manager_app/services/http_service.dart';
+
+class TaskServices {
+  Future<List<Task>> getTasks(int id) async {
+    final response = await httpService.get(
+      Uri.parse(Config.apiURL + "/projects/$id/tasks"),
+    );
+    if (response.statusCode < 400) {
+      final data = List<Map<String, dynamic>>.from(json.decode(response.body));
+      return data.map((task) => Task.fromJson(task)).toList();
+    } else {
+      final data = json.decode(response.body) as Map<String, dynamic>;
+      throw Exception(data["error"]);
+    }
+  }
+
+  Future<bool> postTask(Task task) async {
+    final response = await httpService.post(
+      Uri.parse(
+          Config.apiURL + "/projects/" + task.project.toString() + "/tasks"),
+      body: json.encode(task.toJson()),
+    );
+    final data = json.decode(response.body) as Map<String, dynamic>;
+    if (response.statusCode < 400) {
+      return data["success"];
+    } else {
+      throw Exception(data["message"]);
+    }
+  }
+
+  Future<bool> deleteTask(int id) async {
+    final response = await httpService.delete(
+      Uri.parse(Config.apiURL + "/task/$id"),
+    );
+    final data = json.decode(response.body) as Map<String, dynamic>;
+    if (response.statusCode < 400) {
+      return data["success"];
+    } else {
+      throw Exception(data["message"]);
+    }
+  }
+
+  Future<bool> updateTask(int id) async {
+    final response = await httpService.put(
+      Uri.parse(Config.apiURL + "/task/$id"),
+    );
+    final data = json.decode(response.body) as Map<String, dynamic>;
+    if (response.statusCode < 400) {
+      return data["success"];
+    } else {
+      throw Exception(data["message"]);
+    }
+  }
+}
